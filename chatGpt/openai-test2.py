3f06378ca98f20d4f99298813bd95a85d77d71de
zhanyd <zhanyd@sina.com>
2023-11-11 16:39:00 +0800

    update

--- chatGpt/openai-test2.py
+++ chatGpt/openai-test2.py
@@ -3,7 +3,7 @@ client = OpenAI()
 
 response = client.images.generate(
   model="dall-e-3",
-  prompt="A Iron Man fly into the sky",
+  prompt="A man in the ruins of war, wearing modern combat uniform, cyberpunk style",
   n=1,
   size="1024x1024",
   quality="standard"
--- pythonCrashCourse/chapter4/practiceDictionary.py
+++ pythonCrashCourse/chapter4/practiceDictionary.py
@@ -10,3 +10,34 @@ print(person['age'])
 for key, value in person.items():
     print(f"\nkey : {key}")
     print(f"value : {value}")
+
+pets = [
+    {
+        "type" : "dog",
+        "name" : "Max",
+        "age" : 2
+    },
+    {
+        "type" : "cat",
+        "name" : "Kitty",
+        "age" : 5
+    },
+    {
+        "type" : "fish",
+        "name" : "Sushi",
+        "age" : ""
+    }
+]    
+
+for pet in pets:
+    '''注释'''
+    for key, value in pet.items():
+        print(f"\nkey : {key}")
+        print(f"value : {value}")
+
+def describe_pet(pet_name, pet_type = "dog"):
+    """Display information about a pet."""
+    print(f"\nI have a {pet_type}.")
+    print(f"My {pet_type}'s name is {pet_name}.")
+
+describe_pet("ww")
