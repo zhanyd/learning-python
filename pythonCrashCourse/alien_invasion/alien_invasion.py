1f78d10f55f6d432cae94ded8bd0fa0e0484a02c
zhanyd <zhanyd@sina.com>
2023-11-29 16:48:18 +0800

    update

--- pythonCrashCourse/alien_invasion/alien_invasion.py
+++ pythonCrashCourse/alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     '''管理游戏资源和行为的类'''
@@ -13,22 +14,44 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
 
+        self.ship = Ship(self)
+
         # 设置背景色
         self.bg_color = (230, 230, 230)
 
     def run_game(self):
         '''开始游戏'''
         while True:
-            # 监视键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()    
+            # 响应按键和鼠标事件
+            self._check_events()
+            self.ship.update()
+            # 更新屏幕上的图像，并切换到新屏幕
+            self._update_screen()
 
-            # 每次循环时都重绘屏幕
-            self.screen.fill(self.settings.bg_color)  
+    def _check_events(self): 
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
 
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False   
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False    
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)  
+        self.ship.blitem()
+
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏
--- pythonCrashCourse/alien_invasion/ship.py
+++ pythonCrashCourse/alien_invasion/ship.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 
 class Ship:
@@ -5,3 +6,30 @@ class Ship:
 
     def __init__(self, ai_game):
         '''初始化飞船并设置其初始位置'''
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # 获取当前脚本所在目录
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
+        # 加载飞船图像并获取其外接矩形
+        self.image = pygame.image.load(current_dir + '/images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # 计算飞船的位置，让其居中
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+    def blitem(self):
+        '''在指定位置绘制飞船'''
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        '''根据移动标志更新飞船的位置'''
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
