183f1bd65a0f44fc8310df5279bfc76b28de3a6e
zhanyd <zhanyd@sina.com>
2023-12-03 18:52:10 +0800

    update

--- pythonCrashCourse/alien_invasion/alien_invasion.py
+++ pythonCrashCourse/alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     '''管理游戏资源和行为的类'''
@@ -17,7 +18,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
 
+        self._create_fleet()
         # 设置背景色
         self.bg_color = (230, 230, 230)
 
@@ -69,12 +72,42 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False        
 
+    def _create_fleet(self):
+        '''创建外星人群'''
+        # 创建一个外星人
+        alien = Alien(self)
+        # 外星人的间距为外星人的宽度
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2* alien_width)
+
+        # 计算屏幕可容纳多少行外星人
+        ship_height = self.ship.rect.height
+        available_space_y = self.settings.screen_height - (3 * alien_height) - ship_height
+        numer_rows = available_space_y // (2 * alien_height)
+
+        # 创建外星人群
+        for row_number in range(numer_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        '''创建一个外星人并将其加入当前行'''
+        alien = Alien(self)
+        alien_with = alien.rect.width
+        alien.x = alien_with + 2 * alien_with * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
+
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
         self.screen.fill(self.settings.bg_color)  
         self.ship.blitem()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+
+        self.aliens.draw(self.screen)    
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
