576582ff8980bbecbfc78fd711d607ccc1777679
zhanyd <zhanyd@sina.com>
2024-02-21 20:12:05 +0800

    update

--- pythonCrashCourse/learning_log/users/urls.py
+++ pythonCrashCourse/learning_log/users/urls.py
@@ -6,4 +6,6 @@ app_name = 'users'
 urlpatterns = [
     # 包含默认的身份验证URL模式
     path('', include('django.contrib.auth.urls')),
+    # 注册页面
+    path('register/', views.register, name='register')
 ]
--- pythonCrashCourse/learning_log/users/views.py
+++ pythonCrashCourse/learning_log/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register(request):
+    # 注册新用户
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect('learning_logs:index')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
